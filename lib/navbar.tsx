487c637e02e3efd561c8fb01fc744b0f91afe98f
mecarito <pieritomecar@gmail.com>
2021-08-27 23:09:57 +0300

    added active link support

--- lib/navbar.tsx
+++ lib/navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import LoggedInUser from "../components/loggedInUser";
 import data from "../Dummydata/data.json";
 
@@ -8,6 +8,7 @@ interface Navigation {
 }
 export default function Navbar() {
   const loggedInUser = data.loggedInUser;
+  const selectedlink = useRef(false);
 
   const navigationMenu: Navigation[] = [
     {
@@ -24,15 +25,32 @@ export default function Navbar() {
     },
   ];
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      const output = document.getElementById("Reviews");
+      if (output) output.style.backgroundColor = "#2A585C";
+    }, 500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
   return (
-    <div className="flex flex-row items-center justify-between py-3">
+    <div className="flex flex-row items-center justify-between pt-4 shadow-sm">
       <h1 className="text-black text-2xl font-semibold">AfyaYako</h1>
       <div className="flex flex-row space-x-16">
         <div className="flex flex-row gap-x-8 items-center">
           {navigationMenu.map((data) => {
             return (
-              <li key={data.name} className="list-none ">
-                <a href={data.link}> {data.name}</a>
+              <li key={data.name} className="list-none flex flex-col gap-y-4">
+                <a
+                  href={data.link}
+                  className="hover:text-secondary focus:text-secondary"
+                >
+                  {data.name}
+                </a>
+                <div className="w-full h-0.5 " id={data.name}></div>
               </li>
             );
           })}
